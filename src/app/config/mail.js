5b08260c83ec94576c06975ef5e041dff6bc3e72
Igor Clemente <igorelectrofest2011@>
2019-09-16 09:31:12 -0300

    ROCKETSEAT_CHALLENGES_CHALLENGE03_MEETUPAPP_MAILLIBCLASS_CREATED_16092019

---
+++ src/app/config/mail.js
@@ -0,0 +1,9 @@
+module.exports = {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'e5fdf4668ed222',
+    pass: '0b0f0e204a06f4',
+  },
+};
---
+++ src/lib/Mail.js
@@ -0,0 +1,15 @@
+import nodemailer from 'node-mailer';
+import exphbs from 'express-handlebars';
+import ndmexphbs from 'nodemailer-express-handlebars';
+
+import { resolve } from 'path';
+import mailConfig from
+
+class Main {
+  constructor() {
+
+    this.transporter = nodemailer.createTransport({
+
+    })
+  }
+}
