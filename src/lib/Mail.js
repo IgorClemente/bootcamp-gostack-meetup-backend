7d0ce5453882d1922a5c508125c426bbdac5650e
Igor Clemente <igorelectrofest2011@hotmail.com>
2019-09-16 12:59:57 -0300

    ROCKETSEAT_CHALLENGES_CHALLENGE03_MEETUPAPP_MAILLIBCLASS_UPDATE_16092019

--- src/lib/Mail.js
+++ src/lib/Mail.js
@@ -3,13 +3,31 @@ import exphbs from 'express-handlebars';
 import ndmexphbs from 'nodemailer-express-handlebars';
 
 import { resolve } from 'path';
-import mailConfig from
+import mailConfig from '../app/config/mail';
 
 class Main {
   constructor() {
+    const { host, port, secure, auth } = mailConfig;
+    this.transporter = nodemailer.createTransport({ host, port, secure, auth });
+  }
 
-    this.transporter = nodemailer.createTransport({
+  configureTemplates() {
+    const viewPath = resolve(__dirname, '..', 'app', 'views');
 
-    })
+    this.transporter.use(
+      'compile',
+      ndmexphbs({
+        viewEngine: exphbs.create({
+          layoutsDir: resolve(viewPath, 'layouts'),
+          partialsDir: resolve(viewPath, 'partials'),
+          defaultLayout: 'default',
+          extname: '.hbs',
+        }),
+        viewPath,
+        extName: '.hbs',
+      })
+    );
   }
 }
+
+export default new Main();
